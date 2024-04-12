b89ec6bbc5fc69aa39c1786d3e2c4761f286bad3
spete26 <158122825+spete26@users.noreply.github.com>
2024-04-11 21:57:31 -0500

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -84,7 +84,7 @@ namespace GithubActionsLab
         // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
-            return double.Parse(x) ^ double.Parse(y);
+            return math.pow(double.Parse(x), double.Parse(y));
         }
     }
 
